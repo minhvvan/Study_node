4549ca18ff8245c687fd3295ce21f5d61f308ded
minhvvan <eminhwan12@gmail.com>
2021-01-20 18:46:35 +0900

    inner Object & Module

---
+++ util.js
@@ -0,0 +1,19 @@
+const util = require('util');
+const crypto = require('crypto');
+
+
+const dontUseMe = util.deprecate((x, y) => {
+    console.log(x + y);
+},'deprecated!! do not use!!');
+
+dontUseMe(1, 2);
+
+
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+randomBytesPromise(64)
+    .then((buf) => {
+        console.log(buf.toString('base64'));
+    })
+    .catch((err) => {
+        console.error(err);
+    });
